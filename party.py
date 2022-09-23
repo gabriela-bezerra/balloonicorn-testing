bbe272b7eae553f3be46dd7a3d8d2529c5555170
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-09-22 21:20:59 -0700

    Completed further study

--- party.py
+++ party.py
@@ -7,7 +7,20 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+
+    >>> is_mel('Mel Melitpolski', 'mel_mel@ubermelon.com')
+    True
+
+    >>> is_mel('Melitpolski', 'mel@ubermelon.com')
+    True
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
@@ -16,6 +29,18 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+
+    >>> treats = [{'type':'dessert'},{'type':'dessert'}]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'dessert')
+
+    >>> treats = []
+    >>> most_and_least_common_type(treats)
+    (None, None)
+
+    >>> treats = [{'type':'dessert'},{'type':'dessert'},{'type':'drinks'},{'type':'drinks'}, {'type':'appetizer'}]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'appetizer')
     """
 
     types = {}
